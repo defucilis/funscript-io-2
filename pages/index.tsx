4eed0fd71c5ebdabb194b0a69622c5c8c940e392
Defucilis <defucilis@gmail.com>
2021-10-10 12:03:45 +1100

    feat: Add logo, favicon and original homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,32 @@
+import Image from "next/image";
 import Layout from "components/layout/Layout";
+import useDimensions from "lib/hooks/useDimensions";
 
 export const Home = (): JSX.Element => {
+    const { width, height } = useDimensions();
+
     return (
         <Layout>
-            <h1 className="text-2xl text-white">What a nice website</h1>
-            <p>Hello there!</p>
+            <div className="min-h-mobilemain md:min-h-main grid place-items-center">
+                <div>
+                    <div className="w-full text-center">
+                        <Image
+                            src="/logo512.png"
+                            width={Math.min(width * 0.5, height * 0.25)}
+                            height={Math.min(width * 0.5, height * 0.25)}
+                        />
+                    </div>
+                    <h1 className="w-full text-center text-5xl font-bold">Funscript.io</h1>
+                    <h3 className="w-full text-center text-xl leading-none mt-2">
+                        {"Create Funscripts"}
+                        <br />
+                        {"Modify Funscripts"}
+                        <br />
+                        {"Enjoy Funscripts"}
+                    </h3>
+                </div>
+                <div />
+            </div>
         </Layout>
     );
 };
