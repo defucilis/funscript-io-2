9d1c1ff2782de859963b837376b5e8dd13f94dfa
Defucilis <defucilis@gmail.com>
2023-04-25 12:21:10 +1000

    fix: Resolve URL encoding issue

--- lib/thehandy/HandyUtils.ts
+++ lib/thehandy/HandyUtils.ts
@@ -1,17 +1,17 @@
-import { CsvUploadResponse } from "./types";
+const baseUrl = "https://scripts01.handyfeeling.com/api/script/v0/";
 
 export default class HandyUtils {
     /** Takes a CSV file and uploads it to the publicly-provided Handy server, to obtain a URL that can be sent to the Handy */
-    static async uploadCsv(csv: File, filename?: string): Promise<CsvUploadResponse> {
-        const url = "https://www.handyfeeling.com/api/sync/upload?local=true";
+    static async uploadCsv(csv: File, filename?: string): Promise<string> {
+        const url = baseUrl + "temp/upload";
         if (!filename) filename = "script_" + new Date().valueOf() + ".csv";
         const formData = new FormData();
-        formData.append("syncFile", csv, filename);
+        formData.append("file", csv, filename);
         const response = await fetch(url, {
             method: "post",
             body: formData,
         });
-        const newUrl = await response.json();
+        const newUrl = (await response.json()).url;
         return newUrl;
     }
 }
